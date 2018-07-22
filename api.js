49d699c05381a7db586f906d2bc5765f0294faea
Igor Kamyshev <igor@kamyshev.me>
2018-07-22 19:07:07 +0300

    Added additional information and links

--- api.js
+++ api.js
@@ -5,3 +5,7 @@ const API_BASE = 'https://api.before-i-die.ru'
 export const getFeatured = () => axios
     .get(`${API_BASE}/featured`)
     .then(res => res.data)
+
+export const publishPost = message => axios
+    .post(`${API_BASE}/new`, { message })
+    .then(() => true)
--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
     <section class="panel banner right">
       <div class="content color0 span-3-75">
         <h1 class="major">Прежде чем умру,<br /> 
-        я {{ message }}</h1>
+        я {{ message || '...' }}</h1>
         <ul class="actions">
-          <li><a href="#first" class="button primary color1">Подписаться</a></li>
+          <li><a href="https://t.me/before_i_die" class="button primary color1">Подписаться</a></li>
         </ul>
       </div>
       <div class="image filtered span-1-75" data-position="25% 25%">
@@ -14,7 +14,30 @@
       </div>
     </section>
 
-    <section class="panel color4-alt">
+    <section class="panel spotlight medium right" id="first">
+      <div class="content span-7">
+        <h2 class="major">Почему?</h2>
+        <p>Проект <strong>before I die</strong> зародился в США. Когда художник и архитектор Кэти Чанг потеряла близкого человека, она забила стену одного из заброшенных домов в Новом Орлеане чёрными досками, на которых любой желающий мог продолжить фразу «Before I die I want to...». Уже на следующий день доски были заполнены надписями. Аналогичные сегодня есть в Нидерландах, Новой Зеландии, Мексике, Казахстане, Дании, Парагвае, Китае и многих других странах.</p>
+      </div>
+      <div class="image filtered tinted" data-position="top left">
+        <img src="images/pic02.jpg" alt="" />
+      </div>
+		</section>
+
+    <section class="panel spotlight large left">
+      <div class="content span-5">
+        <h2 class="major">Так почему?</h2>
+        <p>Мы хотели принести в Россию частичку этого проекта, но в силу специфики территории не могли сделать это на улицах и организовали онлайн-стену, куда любой человек может написать свои мысли. Такой формат легко масташируется и почти не требует обслуживания.</p>
+        <ul class="actions">
+          <li><a href="https://t.me/bid_poster_bot" class="button primary color1">Поделиться</a></li>
+        </ul>
+      </div>
+      <div class="image filtered tinted" data-position="top right">
+        <img src="images/pic03.jpg" alt="" />
+      </div>
+    </section>
+
+    <!-- <section class="panel color4-alt">
       <div class="intro color4">
         <h2 class="major">Рассказать</h2>
         <p>Тут можно анонимно поделиться своими мыслями о смысле жизни.</p>
@@ -29,12 +52,14 @@
               </div>
             </div>
             <ul class="actions">
-              <li><input type="submit" value="Опубликовать анонимно" class="button primary" /></li>
+              <li><input type="submit" value="Опубликовать анонимно" class="button primary" @click="publish" /></li>
             </ul>
           </form>
         </div>
       </div>
-    </section>
+    </section> -->
+
+    <div class="copyright">&copy; before I die. Design: <a href="https://html5up.net">HTML5 UP</a>. Images: <a href="https://unsplash.com/">Unsplash</a>.</div>
 
   </div>
 </template>
@@ -42,10 +67,27 @@
 <script>
 import { mapGetters } from 'vuex'
 
+import { publishPost } from '~/api'
+
 export default {
+  // data: () => ({
+  //   requestInProgress: false,
+  //   newPost: '',
+  // }),
   computed: mapGetters([
     'message',
   ]),
+  // methods: {
+  //   publish: function () {
+  //     this.requestInProgress = true
+
+  //     publishPost(this.newPost)
+  //       .then(() => {
+  //         this.requestInProgress = false
+  //         this.newPost = ''
+  //       })
+  //   }
+  // }
 }
 </script>
 
